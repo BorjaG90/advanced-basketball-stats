c54bea64a07f42b089dd7548a7f074e83eaf1266
Borja Gete <borjag90dev@gmail.com>
2020-10-29 16:53:22 +0100

    feat: Add PIR #2

--- test/app.js
+++ test/app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const fakeStatsGenerator = require('fake-stats-generator');
 const dataHandler = require('../lib/handler');
 const initialData = require('./data');
+const app = require('../lib/main');
 
 
 // Datos obtenidos del uso de fake-stats-generator
@@ -11,8 +12,14 @@ const initialData = require('./data');
 const seasonsData = require('../data/output.js');
 
 // Tests
+// Transformations
 let player3stats = dataHandler.transformToPlayer(seasonsData, 3);
 writeOutputFile('player3stats', player3stats);
+
+// Calculations
+let game2stats = player3stats.stats[1]
+let player3game2PIR = app.getTendexPIR(game2stats);
+console.log(`Player 3 Game 2 PIR calculation: ${player3game2PIR}`);
 
 
 // Functions

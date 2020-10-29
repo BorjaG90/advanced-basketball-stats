5939598d0a150df6ea023ec6eb6485ca7bb72753
Borja Gete <borjag90dev@gmail.com>
2020-10-29 16:13:44 +0100

    feat: Add player data structure transform #1

--- test/app.js
+++ test/app.js
@@ -1,3 +1,25 @@
+const fs = require('fs');
 const fakeStatsGenerator = require('fake-stats-generator');
+const dataHandler = require('../lib/handler');
 const initialData = require('./data');
-const seasonsData = fakeStatsGenerator.start(initialData);
+
+
+// Datos obtenidos del uso de fake-stats-generator
+// const seasonsData = fakeStatsGenerator.start(initialData);
+
+// Datos desde un json ya creado
+const seasonsData = require('../data/output.js');
+
+// Tests
+let player3stats = dataHandler.transformToPlayer(seasonsData, 3);
+writeOutputFile('player3stats', player3stats);
+
+
+// Functions
+function writeOutputFile(nameFile, data){
+  fs.writeFile (`${nameFile}.json`, JSON.stringify(data), function(err) {
+    if (err) throw err;
+    console.log(`File ${nameFile} Write complete`);
+    }
+  );
+};

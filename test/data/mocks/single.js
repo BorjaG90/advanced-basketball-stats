8115f0f9d0b64ab4b67378179a094a4bf4a166b6
Borja Gete <borjag90dev@gmail.com>
2020-10-30 14:03:27 +0100

    test: Add mocks #5

---
+++ test/data/mocks/single.js
@@ -0,0 +1,22 @@
+const singleGame = {
+  "2pm": 1,
+  "3pm": 3,
+  ftm: 4,
+  "2pa": 15,
+  "3pa": 5,
+  fta: 7,
+  offReb: 1,
+  defReb: 10,
+  ass: 3,
+  ste: 1,
+  blk: 0,
+  tos: 1,
+  foul: 0,
+  foulDrawn: 1,
+  shotReject: 1,
+  min: 37,
+};
+
+module.exports = {
+  singleGame
+}
